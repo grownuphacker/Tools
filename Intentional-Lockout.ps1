e59d31ad61c303ccb68d96846fbf69e65a43f121
Big Abe <mr_abernethy@outlook.com>
2018-01-26 01:42:53 -0500

    This is good stuff

--- Intentional-Lockout.ps1
+++ Intentional-Lockout.ps1
@@ -1,3 +1,15 @@
+#  Created by @ReallyBigAbe for BlueTeam.Ninja
+#  I jacked the basics from http://mikefrobbins.com/2013/11/28/lock-out-active-directory-user-accounts-with-powershell/
+#  I'm not a DEV, so I have no idea what the protocol is here
+
+#  Basically just throw in an Account name, and this will dig up your lockout policy, smash the account until its locked, and carry on with life. 
+#  Good for testing various triggers. 
+
+#  Only use it on friends, suspicious co-workers, and your boss's boss.  Anything else isn't funny enough.
+
+
+
+
 [CmdletBinding()]
 Param(
     [Parameter(Mandatory=$true)]
@@ -8,7 +20,6 @@ Param(
 $Password = ConvertTo-SecureString 'Not Really My Password' -AsPlainText -Force
 
 #Scrape the Lockout requirements
-#Pilfered from http://mikefrobbins.com/2013/11/28/lock-out-active-directory-user-accounts-with-powershell/
 if ((([xml](Get-GPOReport -Name "Default Domain Policy" -ReportType Xml)).GPO.Computer.ExtensionData.Extension.Account |
 Where-Object name -eq LockoutBadCount).SettingNumber) {
 
