c8acce41c69799d32b43acb4ac70371555922111
Big Abe <mr_abernethy@outlook.com>
2018-01-26 01:29:44 -0500

    Adding Lockout Tool

---
+++ Intentional-Lockout.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$true)]
+        [string]$Account
+)
+
+#Set a garbage password
+$Password = ConvertTo-SecureString 'Not Really My Password' -AsPlainText -Force
+
+#Scrape the Lockout requirements
+#Pilfered from http://mikefrobbins.com/2013/11/28/lock-out-active-directory-user-accounts-with-powershell/
+if ((([xml](Get-GPOReport -Name "Default Domain Policy" -ReportType Xml)).GPO.Computer.ExtensionData.Extension.Account |
+Where-Object name -eq LockoutBadCount).SettingNumber) {
+
+
+if(Test-Connection $env:LOGONSERVER) {$dc = $env:LOGONSERVER}
+elseif(Test-Connection $env:USERDOMAIN) {$dc = $env:USERDOMAIN}
+else {$dc = $null; Write-Output "No DCs to mess with"   return 1;}
+
+Get-ADUser $Account -Properties SamAccountName, UserPrincipalName, LockedOut |
+Do {
+
+    Invoke-Command -ComputerName $dc {Get-Process
+    } -Credential (New-Object System.Management.Automation.PSCredential ($($_.UserPrincipalName), $Password)) -ErrorAction SilentlyContinue
+
+}
+Until ((Get-ADUser -Identity $_.SamAccountName -Properties LockedOut).LockedOut)
+
+Write-Output "$($_.SamAccountName) has been locked out"
+}else{
+    Write-Output "There's no lockout policy under `"Default Domain Policy`".  You might want to look into that, chief."
+}
+
